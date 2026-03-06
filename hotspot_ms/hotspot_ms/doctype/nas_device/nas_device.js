d3b16f275ff74fa41fefee5a214061281e156e1a
Sydney Kibanga <skibanga@aakvatech.com>
2026-03-06 16:25:35 +0300

    feat(hotspot_ms): add core hotspot doctypes with UX-focused layouts and migrate schema

---
+++ hotspot_ms/hotspot_ms/doctype/nas_device/nas_device.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Sydney Kibanga and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Nas Device", {
+// 	refresh(frm) {
+//
+// 	},
+// });
---
+++ hotspot_ms/hotspot_ms/doctype/radius_attribute/radius_attribute.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Sydney Kibanga and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Radius Attribute", {
+// 	refresh(frm) {
+//
+// 	},
+// });
