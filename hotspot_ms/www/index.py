6c23c7a4818624b4f93d213ac7ae9f8bdc3c616c
Sydney Kibanga <skibanga@aakvatech.com>
2026-03-23 12:07:06 +0300

    fix(web): stop overriding /login; move hotspot redirect logic to /portal

---
+++ hotspot_ms/www/index.py
@@ -0,0 +1,8 @@
+import frappe
+
+
+def get_context(context):
+	context.no_cache = 1
+	context.title = "Tanzania Hotspot"
+	context.current_year = frappe.utils.now_datetime().year
+	return context
--- hotspot_ms/www/portal.py
+++ hotspot_ms/www/portal.py
@@ -3,6 +3,6 @@ import frappe
 
 def get_context(context):
 	context.no_cache = 1
-	context.title = "Login Redirect"
+	context.title = "Portal Redirect"
 	context.current_year = frappe.utils.now_datetime().year
 	return context
