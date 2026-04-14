5548160e84af742d32c781795d34f05299c40af0
Sydney Kibanga <skibanga@aakvatech.com>
2026-04-14 20:39:03 +0300

    feat(nas-device): add in-desk generator for openNDS FAS shared key

--- hotspot_ms/hotspot_ms/doctype/nas_device/nas_device.js
+++ hotspot_ms/hotspot_ms/doctype/nas_device/nas_device.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2026, Sydney Kibanga and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Nas Device", {
-// 	refresh(frm) {
-//
-// 	},
-// });
+frappe.ui.form.on("Nas Device", {
+	refresh(frm) {
+		if (frm.is_new()) return;
+
+		frm.add_custom_button(__("Generate FAS Key"), async () => {
+			const result = await frappe.call({
+				method: "hotspot_ms.hotspot_ms.doctype.nas_device.nas_device.generate_opennds_fas_key",
+				args: { name: frm.doc.name },
+			});
+
+			const key = result.message && result.message.opennds_fas_key;
+			if (!key) {
+				frappe.msgprint(__("Could not generate FAS key."));
+				return;
+			}
+
+			await frm.reload_doc();
+			frappe.msgprint({
+				title: __("FAS Key Generated"),
+				indicator: "green",
+				message: __("The openNDS FAS key has been generated and saved on this NAS Device."),
+			});
+		});
+	},
+});
--- hotspot_ms/hotspot_ms/doctype/nas_device/nas_device.py
+++ hotspot_ms/hotspot_ms/doctype/nas_device/nas_device.py
@@ -1,8 +1,28 @@
 # Copyright (c) 2026, Sydney Kibanga and contributors
 # For license information, please see license.txt
 
+import secrets
+
+import frappe
 from frappe.model.document import Document
 
 
 class NasDevice(Document):
 	pass
+
+
+@frappe.whitelist()
+def generate_opennds_fas_key(name: str | None = None) -> dict:
+	"""
+	Generate a new shared secret for openNDS secure FAS.
+	Stores it on the Nas Device record when a document name is provided.
+	"""
+	key = secrets.token_hex(32)
+
+	if name:
+		doc = frappe.get_doc("Nas Device", name)
+		doc.opennds_fas_key = key
+		doc.save(ignore_permissions=True)
+		frappe.db.commit()
+
+	return {"ok": True, "opennds_fas_key": key}
