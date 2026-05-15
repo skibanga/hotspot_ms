3d0bcfa20ce0d9bb6a88cfa902ed43bd642a4dbe
Sydney Kibanga <skibanga@aakvatech.com>
2026-05-16 02:12:37 +0300

    feat: inject lang into context and update navigation template logic to use it

--- hotspot_ms/www/hotspot/login.py
+++ hotspot_ms/www/hotspot/login.py
@@ -7,6 +7,7 @@ from hotspot_ms.branding import get_portal_branding
 def get_context(context):
 	lang = frappe.request.cookies.get("lang") or "sw"
 	frappe.local.lang = lang
+	context.lang = lang
 
 	context.no_cache = 1
 	context.active_page = "login"
