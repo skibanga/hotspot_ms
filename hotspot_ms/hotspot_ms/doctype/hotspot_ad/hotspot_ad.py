ef55c6b7438acf9ca408ec08df1ef1e6595b28c9
Sydney Kibanga <skibanga@aakvatech.com>
2026-05-17 18:18:34 +0300

    feat: introduce Hotspot Ad doctype and enhance free access logic with chunked sessions and ad integration

---
+++ hotspot_ms/hotspot_ms/doctype/hotspot_ad/hotspot_ad.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2026, Sydney Kibanga and contributors
+# For license information, please see license.txt
+
+from __future__ import annotations
+
+import frappe
+from frappe.model.document import Document
+
+
+class HotspotAd(Document):
+	pass
